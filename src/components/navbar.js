347ea8b46a3927d74fe2345fd97a3b433ab7be0a
vinodpatidar123 <pvinod813@gmail.com>
2020-05-05 18:04:52 +0530

    context API

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useContext  } from 'react'
+import { TodoContext } from "../TodoContext";
 
 export default function Navbar() {
+  const [todos, setTodos] = useContext(TodoContext)
   const [menu, setMenu] = useState(false)
+  const [value, setValue] = useState("")
+
+  const addTodo = (e) => {
+    e.preventDefault()
+    setTodos([...todos, {
+      "id": Date.now(),
+      "value": value,
+      "done": false
+    }])
+  }
 
   return (
     <div className="navbar has-shadow">
@@ -21,14 +33,16 @@ export default function Navbar() {
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="field has-addons">
-              <p className="control is-expanded">
-                <input type="text" className="input" />
-              </p>
-              <p className="control">
-                <button className="button is-info has-text-weight-bold">
-                  Add Todo
-                </button>
-              </p>
+               <form onSubmit={addTodo} className="field has-addons">
+        <p className="control is-expanded">
+          <input value={value} type="text" onChange={(e) => setValue(e.target.value)} className="input" />
+        </p>
+        <p className="control">
+          <button className="button is-info has-text-weight-bold">
+            Add Todo
+          </button>
+        </p>
+      </form>
             </div>
           </div>
         </div>
